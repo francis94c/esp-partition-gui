de453875f41308dae61c6d29a14672cdfe56aac9
Francis-PC\Francis <francis94c@gmail.com>
2017-12-21 10:09:21 +0100

    Add and remove Button GUI

--- esp-partition.py
+++ esp-partition.py
@@ -3,6 +3,11 @@ from Tkinter import *
 
 class ESPPartitionGUI(Frame):
     def __init__(self, master=None):
+        """
+        Initialization of the Main GUI Form
+        The Widgets are loaded and arranged here with the default ESP partition template.
+        :param master: a Tk top level object obtained by calling Tk().
+        """
         Frame.__init__(self, master)
         self.pack(fill=BOTH, side=TOP, expand=True)
         self.grid_columnconfigure(0, weight=1)
@@ -14,28 +19,39 @@ class ESPPartitionGUI(Frame):
         self.sub_type_int_var = IntVar()
         self.sub_type_checkbox = Checkbutton(self, text="Enable", variable=self.sub_type_int_var,
                                              command=self.toggle_sub_type).grid(
-            row=0, column=2)
-        self.offset_checkbox = Checkbutton(self, text="Enable", variable=IntVar(), command=self.toggle_sub_type).grid(
             row=0, column=3)
-        self.size_checkbox = Checkbutton(self, text="Enable", variable=IntVar(), command=self.toggle_sub_type).grid(
+        self.offset_checkbox = Checkbutton(self, text="Enable", variable=IntVar(), command=self.toggle_sub_type).grid(
             row=0, column=4)
-        self.flags_checkbox = Checkbutton(self, text="Enable", variable=IntVar(), command=self.toggle_sub_type).grid(
+        self.size_checkbox = Checkbutton(self, text="Enable", variable=IntVar(), command=self.toggle_sub_type).grid(
             row=0, column=5)
-        Label(self, text="Name").grid()
-        Label(self, text="Type").grid(row=1, column=1)
-        Label(self, text="SubType").grid(row=1, column=2)
-        Label(self, text="Offset").grid(row=1, column=3)
-        Label(self, text="Size").grid(row=1, column=4)
-        Label(self, text="Flags").grid(row=1, column=5)
+        self.flags_checkbox = Checkbutton(self, text="Enable", variable=IntVar(), command=self.toggle_sub_type).grid(
+            row=0, column=6)
+        self.controls = {"name": [], "ar_buttons": {}}
+        for i in range(6):
+            b = Button(self, text="-")
+            b.grid(row=2 + i, column=0)
+            self.controls["ar_buttons"]["button_{}".format(i)] = b
+        b = Button(self, text="+")
+        b.grid(row=8, column=0)
+        self.last_row = 8
+        Label(self, text="Name").grid(row=1, column=2)
+        Label(self, text="Type").grid(row=1, column=3)
+        Label(self, text="SubType").grid(row=1, column=4)
+        Label(self, text="Offset").grid(row=1, column=5)
+        Label(self, text="Size").grid(row=1, column=6)
+        Label(self, text="Flags").grid(row=1, column=7)
         self.ui_entries = {"name_0": StringVar(), "name_1": StringVar(), "name_2": StringVar(), "name_3": StringVar(),
                            "name_4": StringVar(), "name_5": StringVar()}
-        self.controls = {"name": []}
         for i in range(6):
             e = Entry(self, textvariable=self.ui_entries["name_{}".format(i)])
-            e.grid(row=2 + i, column=0)
+            e.grid(row=2 + i, column=2)
             self.controls["name"].append(e)
         self.ui_entries["name_0"].set("nvs")
         self.ui_entries["name_1"].set("otadata")
+        self.ui_entries["name_2"].set("app0")
+        self.ui_entries["name_3"].set("app1")
+        self.ui_entries["name_4"].set("eeprom")
+        self.ui_entries["name_5"].set("spiffs")
 
     def toggle_sub_type(self):
         enable = self.sub_type_int_var.get()
